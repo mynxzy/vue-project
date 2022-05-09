7472a7a043760f66b44cbeb4d461d11e84594916
DESKTOP-H3NI615\minji <alswl7742@naver.com>
2022-05-10 00:09:39 +0900

    화면라우팅설정

--- vue_front/src/App.vue
+++ vue_front/src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <!-- 추가된 헤더 컴포넌트 사용 -->
     <PageHeader />
-    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-	
+    <router-view/>
     <!-- 추가된 풋터 컴포넌트 사용 -->
     <PageFooter />
   </div>
@@ -12,14 +10,12 @@
 
 <script>
 import PageHeader from './components/common/PageHeader.vue'; //import 헤더 추가
-import HelloWorld from './components/HelloWorld.vue'; 
 import PageFooter from './components/common/PageFooter.vue'; //import 풋터 추가
 
 export default {
   name: 'App',
   components: {
     PageHeader          //헤더 컴포넌트 추가
-    , HelloWorld
     ,PageFooter         //풋터 컴포넌트 추가
     
   }
