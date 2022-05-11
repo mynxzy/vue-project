02a4188855e901ea41538730b49e85d809a214c8
zero <adgjl1125@naver.com>
2022-05-11 23:36:31 +0900

    vue-good-table lib 사용

--- vue_front/src/components/board/BoardList.vue
+++ vue_front/src/components/board/BoardList.vue
@@ -2,43 +2,27 @@
 	<div>
 		<h2>멤버 리스트</h2>
 		조회된 데이터 엑셀 다운로드 기능 구현
-		<table>
-			<tr>
-				<th>no</th>
-				<th>id</th>
-				<th>memberNm</th>
-				<th>silverAddr</th>
-				<th>auth</th>
-				<th>category</th>
-				<th>memberHp</th>
-				<th>cretDt</th>
-			</tr>
-			<tr v-for="item in tableData" :key="item">
-				<td>{{item.no}}</td>
-				<td>{{item.id}}</td>
-				<td>{{item.memberNm}}</td>
-				<td>{{item.silverAddr}}</td>
-				<td>{{item.auth}}</td>
-				<td>{{item.category}}</td>
-				<td>{{item.memberHp}}</td>
-				<td>{{item.cretDt}}</td>
-			</tr>
-    	</table>
+		<vue-good-table
+			:columns="tableFields"
+			:rows="tableData"/>
 	</div>
 </template>
 
 <script>
+import { VueGoodTable } from 'vue-good-table-next';
 import {tableData} from "./exampleData.js";
+import {tableFields} from "./fields.js";
+import 'vue-good-table-next/dist/vue-good-table-next.css'
 export default {
 	components: {
-	
+		VueGoodTable
 	},
 	created(){
-		alert(this.tableData[0].memberNm);
 	},
 	data() {
 		return {
-			tableData:tableData
+			tableData:tableData,
+			tableFields: tableFields
 		};
 	},
 	methods:{
---
+++ vue_front/src/components/board/fields.js
@@ -0,0 +1,36 @@
+const tableFields=[
+    {
+        label: '순번',
+        field: 'no'
+    },
+    {
+        label: 'ID',
+        field: 'id'
+    },
+    {
+        label: '이름',
+        field: 'memberNm'
+    },
+    {
+        label: '주소',
+        field: 'silverAddr'
+    },
+    {
+        label: '권한',
+        field: 'auth'
+    },
+    {
+        label: '구분',
+        field: 'category'
+    },
+    {
+        label: '휴대폰 번호',
+        field: 'memberHp'
+    },
+    {
+        label: '생성일',
+        field: 'cretDt'
+    }
+];
+
+export {tableFields};
