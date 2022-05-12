b17871fdb9bc221d1b1e33200bfe587b93b80781
mynxzy <88770029+mynxzy@users.noreply.github.com>
2022-05-12 13:49:11 +0900

    차트생성

--- vue_front/src/components/chart/member-chart.vue
+++ vue_front/src/components/chart/member-chart.vue
@@ -1,19 +1,19 @@
 <template>
 	<div>
 		<h2>일자별 회원 등록 수 기록하는 화면</h2>
-		<ul>
-			<li v-for="item in chartData" v-bind:key="item">
-				{{item.cretDt}} / {{item.cnt}}
-			</li>
-		</ul>
+		<Bar :chart-data="chartData" />
 	</div>
 </template>
 
 <script>
 import {chartData} from "./exampleData.js";
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
 	components: {
-
+		Bar
 	},
 	created(){
 		//화면 진입 시, 제일 먼저 실행되는 함수
@@ -21,12 +21,29 @@ export default {
 	data() {
 		//데이터 객체 반환 함수
 		return {
-			chartData:chartData
+			chartData:{
+				labels: [
+					
+					],
+				datasets: [
+					{
+					label: 'cnt',
+					backgroundColor: '#f87979',
+					data: [
+						chartData[0].cnt, chartData[1].cnt, chartData[2].cnt, chartData[3].cnt, chartData[4].cnt, chartData[5].cnt, chartData[6].cnt
+						]
+					}
+				]
+			}
 		};
 	},
 	methods:{
 		//함수 기재
-		
+		date(){
+			for(var i=0; i<chartData.length; i++){
+				return this.chartData[i].cretDt;
+			}
+		}
 	}
 };
 </script>
